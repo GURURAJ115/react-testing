c86bc72b8fd22cb62fc846bda98a1f8392049987
GURURAJ115 <gururajsnaik115@gmail.com>
2024-07-02 21:32:42 +0530

    Basic Unit testing fundamentals

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Demo Information</h2>
+      <input type="text" placeholder="Enter email"/>
+      <button>Click here</button>
+      <ul>
+        <li>Phone</li>
+        <li>Laptop</li>
+      </ul>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// *.test.js OR *.spec.js OR *.js(__tests__) Folder
+// it("example test one", ()=>{})
+
+  test("example test one", ()=>{
+    render(<App/>);
+    //test whether button exists or not 
+    // So first get the button 
+    const btnElement = screen.getByRole("button",{name: "Click here", exact: false})
+    const btnElement2 = screen.queryByRole("button",{name: "Click now", exact: false})
+    // perform assertion 
+    expect(btnElement).toBeInTheDocument();
+    expect(btnElement2).toBeNull();
+    expect(btnElement2).not.toBeInTheDocument();
+})
